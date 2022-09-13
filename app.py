a83ce09f971cae3573ba9d951ecb92d7d8063366
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-09-13 12:21:25 +0800

    GitIgnore Added

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
+from livereload import Server
 
+config = {
+    "DEBUG": True,  # run app in debug mode
+    'TEMPLATES_AUTO_RELOAD': True
+}
 
 app = Flask(__name__)
 
 
 @app.route('/hello')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config.from_mapping(config)
+
+    server = Server(app.wsgi_app)
+    # server.watch
+    server.serve()
